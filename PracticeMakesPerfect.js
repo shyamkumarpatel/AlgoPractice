bb443c5ee13712e28ce33a6c9b68ae43620d6daa
Shyam Patel <shyam.psu.lv@gmail.com>
2022-06-01 02:28:48 -0400

    given a string of word in reverse order, this script set them in correct order.

--- PracticeMakesPerfect.js
+++ PracticeMakesPerfect.js
@@ -1,12 +1,33 @@
-var inverseString = "Perfect Makes Practice";
+/*
+*  given a string with the words in reverse order, let's correct the order so that it's "Practice Makes Perfect" 
+*/
+var outOfOrderString = "Perfect Makes Practice";
+console.log(outOfOrderString);
 
-
+// temp variable to hold the reverse of outOfOrderString
 var temp = "";
 
-for(var i = inverseString.length-1; i >= 0; i--){
-    temp += inverseString.charAt(i);
-    console.log(i);
+for(var i = outOfOrderString.length-1; i >= 0; i--){
+    temp += outOfOrderString.charAt(i);
 }
-
-console.log(inverseString);
 console.log(temp);
+
+//let's inverse the each word with temp string so that it read correctly.
+var correctString = "";
+var indexAfterLastWhiteSpace = 0;
+
+for(var i = 0; i < temp.length; i++){
+    if(temp.charAt(i) == " "){
+        for(var j = i-1; j >=indexAfterLastWhiteSpace; j--){
+            correctString += temp.charAt(j);
+        }
+        correctString += " ";
+        indexAfterLastWhiteSpace = i+1;
+    }
+    else if(i == temp.length-1){
+        for(var j = i; j >=indexAfterLastWhiteSpace; j--){
+            correctString += temp.charAt(j);
+        }
+    }
+}
+console.log(correctString);
