2d27f453451c5508138978c2466c1be12e6f9c62
Shyam Patel <shyam.psu.lv@gmail.com>
2022-06-01 19:22:29 -0400

    Running Sum implementation in JS

---
+++ RunningSumOf1dArray.js
@@ -0,0 +1,20 @@
+/*  Given an array nums.
+ *   We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+ *   Return the running sum of nums. */
+
+function runningSums(nums) {
+    var runningSums = [];
+    if (nums.length <= 1) {
+        return nums;
+    } else {
+        runningSums.push(nums[0]);
+        for (var i = 1; i < nums.length; i++) {
+            runningSums.push(nums[i] + runningSums[i - 1]);
+        }
+        return runningSums;
+  }
+}
+
+let nums = [1, 2, 3, 4];
+console.log(nums);
+console.log(runningSums(nums));
