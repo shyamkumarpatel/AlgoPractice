b201a86906baf3546593e28c42f683787b320d60
Shyam Patel <shyamkumarpatel@users.noreply.github.com>
2022-06-04 02:29:54 -0400

    Update PalindromeNumber.js
    removing console.log

--- PalindromeNumber.js
+++ PalindromeNumber.js
@@ -15,14 +15,3 @@
     //console.log("-----x=",x, "reverseX=", reverseX);
     return reverseX === x;
 };
-console.log(1, isPalindrome(-1));
-console.log(1, isPalindrome(1));
-console.log(10, isPalindrome(10));
-console.log(11, isPalindrome(11));
-console.log(102, isPalindrome(102));
-console.log(101, isPalindrome(101));
-console.log(1112, isPalindrome(1112));
-console.log(1001, isPalindrome(1001));
-console.log(1111, isPalindrome(1111));
-console.log(11112, isPalindrome(11112));
-console.log(11111, isPalindrome(11111));
