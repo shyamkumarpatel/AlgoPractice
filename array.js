ed008623f51a243d6eebe2f093a63ba89bcbf001
Shyam Patel <shyam.psu.lv@gmail.com>
2022-07-18 17:50:36 -0400

    uploading Jul 18 Algo practice

---
+++ array.js
@@ -0,0 +1,12 @@
+var checkTree = function(root) {
+    console.log(root[0]);
+    console.log(root[1]);
+    console.log(root[2]);
+    return root[0] ===(root[1]+root[2] );
+};
+
+
+
+
+
+console.log(checkTree([10,4,6]));
