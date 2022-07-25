d74461a21f084d7fecfab3bfa773646da3e9e839
Shyam Patel <shyam.psu.lv@gmail.com>
2022-07-25 11:46:02 -0400

    algo practice

---
+++ Jul25/W4D1.js
@@ -0,0 +1,135 @@
+/* 
+  Given to me (Neil) in an interview
+  Given a string
+  return whether or not it's possible to make a palindrome out of the string's
+  characters.
+  What is it about a string that makes it possible for it to become a
+  palindrome?
+*/
+
+const str1 = "";
+const expected1 = false;
+
+const str2 = "a";
+const expected2 = true;
+
+const str3 = "ddaa";
+const expected3 = true;
+// Explanation: "daad" or "adda"
+
+const str4 = "dda";
+const expected4 = true;
+// Explanation: "dad"
+
+const str5 = "aaadd";
+const expected5 = true;
+// Explanation: "daaad" / "adada"
+
+const str6 = "abc";
+const expected6 = false;
+// tacocat
+// racecar
+
+// even number of odd characters
+
+/**
+ * Determines whether or not it is possible for the string's characters to be
+ * rearranged into a palindrome.
+ * - Space: O(?).
+ * - Time: O(?).
+ * @param {string} str
+ * @returns {boolean} Whether the given str can be rearranged into a palindrome.
+ */
+function canStringBecomePalindrome(str) {
+  if(str.length < 1) return false;
+  let new_dict1 = {};
+  let rtBool = false;
+
+  for (let i = 0; i < str.length; i++){
+    if (new_dict1.hasOwnProperty(str.substring(i,i+1))){
+        new_dict1[str.substring(i,i+1)]++;
+    } else{
+        new_dict1[str.substring(i,i+1)] = 1;
+    }
+}
+
+    //console.log("new_dict1 -->",new_dict1);
+    let hasOne = false;
+    let hasOddPairs = false;
+    let hasEvenPairs = false;
+    for (key in new_dict1){
+        if (new_dict1[key] === 1){
+            hasOne = true;
+        }
+        else if(new_dict1[key]%2 !== 0){
+            hasOddPairs = true;
+        }
+        else if(new_dict1[key]%2 === 0){
+            hasEvenPairs = true;
+        }
+    }
+    if(hasOne && str.length==1) rtBool = true;
+    else if(hasEvenPairs){
+        rtBool = true;
+    }
+  return rtBool;
+}
+console.log("Expected: ",expected1);
+console.log("Acutal:   ",canStringBecomePalindrome(str1));
+console.log("");
+console.log("Expected: ",expected2);
+console.log("Acutal:   ",canStringBecomePalindrome(str2));
+console.log("");
+console.log("Expected: ",expected3);
+console.log("Acutal:   ",canStringBecomePalindrome(str3));
+console.log("");
+console.log("Expected: ",expected4);
+console.log("Acutal:   ",canStringBecomePalindrome(str4));
+console.log("");
+console.log("Expected: ",expected5);
+console.log("Acutal:   ",canStringBecomePalindrome(str5));
+console.log("");
+console.log("Expected: ",expected6);
+console.log("Acutal:   ",canStringBecomePalindrome(str6));
+
+
+
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/drop-it
+
+/*
+  Input: arr, callback
+  Output: arr (with elements removed)
+  Remove every element in the array, starting from idx 0,
+  until the callback function returns true when passed the
+  iterated element.
+  Return an empty array if the callback never returns true
+*/
+
+const nums1 = [1, 4, 3, 6, 9, 15];
+const callback1 = (elem) => {
+  return elem > 5;
+};
+//const expected1 = [6, 9, 15];
+
+const nums2 = [...nums1];
+const callback2 = (elem) => {
+  return elem > 2;
+};
+//const expected2 = [4, 3, 6, 9, 15];
+
+const nums3 = [...nums1];
+const callback3 = (elem) => false;
+//const expected3 = [];
+
+/**
+ * Removes every element in the array, starting from idx 0 until the callback
+ * function returns true when passed the iterated element.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<any>} arr
+ * @callback cb A callback function that expects to receive an array element.
+ * @returns {Array<any>} The given array with only the remaining items.
+ */
+function dropIt(arr, cb) {}
